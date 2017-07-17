d0c235a04edbf7eb679b937d64f6668e1e963d09
debashisnath <debashisnath1992@gmail.com>
2017-07-17 23:43:19 +0530

    updated sql

--- portme.sql
+++ portme.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 15, 2017 at 10:23 AM
+-- Generation Time: Jul 17, 2017 at 06:12 PM
 -- Server version: 5.1.36
 -- PHP Version: 5.3.0
 
@@ -50,7 +50,7 @@ INSERT INTO `company_details` (`company_id`, `category_name`, `email`, `work_pho
 
 CREATE TABLE IF NOT EXISTS `customers` (
   `customer_id` int(255) NOT NULL AUTO_INCREMENT,
-  `salutation` enum('1','2','3','4','5') NOT NULL,
+  `salutation` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `company_name` varchar(255) NOT NULL,
@@ -80,7 +80,99 @@ CREATE TABLE IF NOT EXISTS `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `salutation`, `firstname`, `lastname`, `company_name`, `email`, `work_phone`, `mobile`, `website`, `billing_street`, `billing_city`, `billing_state`, `billing_zip`, `billing_country`, `shipping_street`, `shipping_city`, `shipping_state`, `shipping_zip`, `shipping_country`, `notes`, `date`, `added_by`, `business_id`) VALUES
-(2, '1', 'Debashis', 'Nath', 'PORT', 'portme@bp.com', '123456', 'Debashis', 'portme@bp.in', 'santibazaar', 'barrackpore', 'west', '700120', 'INDIA', 'santibazaar', 'barrackpore', 'west', '700120', 'INDIA', '', '', '', '4');
+(2, 'Mrs.', 'Debashis', 'Nath', 'PORT', 'portme@bp.com', '123456', 'Debashis', 'portme@bp.in', 'santibazaar', 'barrackpore', 'west', '700120', 'INDIA', 'santibazaar', 'barrackpore', 'west', '700120', 'INDIA', '', '', '', '4'),
+(4, 'Mr', 'krishanu', 'mondal', 'portme', 'k@gmail.com', '12345', '654132', 'krish.com', 'asdfg', 'asdfg', 'asdfg', '12345', 'india', 'asdfg', 'asdfg', 'asdfg', '12345', 'india', '', '', '', '4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_id` int(255) NOT NULL AUTO_INCREMENT,
+  `product_type` varchar(255) NOT NULL,
+  `category_id` int(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `description` longtext NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `tax_name` varchar(255) NOT NULL,
+  `tax_rate` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `attribute_value` varchar(255) NOT NULL,
+  `attribute_option` varchar(255) NOT NULL,
+  `business_id` int(255) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_type`, `category_id`, `product_name`, `description`, `quantity`, `tax_name`, `tax_rate`, `price`, `attribute_value`, `attribute_option`, `business_id`) VALUES
+(1, 'Product', 1, '0', 'VIBE K5 NOTE', 150, '', '', '', 'COLOR,COLOR', 'RED,BLACK', 4),
+(2, 'Product', 4, 'MORPHI', 'ITS A RADIO', 25, 'CGST', '15', '10000', 'color,type', 'red,radio', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category`
+--
+
+CREATE TABLE IF NOT EXISTS `product_category` (
+  `category_id` int(255) NOT NULL AUTO_INCREMENT,
+  `category_type` varchar(255) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `description` longtext NOT NULL,
+  `tax_name` varchar(255) NOT NULL,
+  `tax_rate` varchar(255) NOT NULL,
+  `attribute_name` longtext NOT NULL,
+  `attribute_options` longtext NOT NULL,
+  `item_type` varchar(255) NOT NULL,
+  `business_id` int(255) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`category_id`, `category_type`, `category_name`, `description`, `tax_name`, `tax_rate`, `attribute_name`, `attribute_options`, `item_type`, `business_id`) VALUES
+(1, '', 'MOBILES', 'ALL TYPES OF MOBILES', 'GST', '15', 'MOBILE PHONES', 'RED', '', 0),
+(2, '', 'MOBILES', 'ALL MOBILES', 'CGST', '28', 'MOBILE 1,MOBILE 2', 'RED,BLACK', '', 0),
+(3, '', 'TELEVISION', 'NEW ITEM', 'CGST', '20', 'COLOR,COLOR', 'BLACK,WHITE', '', 0),
+(4, 'Product', 'RADIO', 'FM', 'ST', '15', 'hey,hello', 'red,yellow', 'Non-inventory', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff_details`
+--
+
+CREATE TABLE IF NOT EXISTS `staff_details` (
+  `staff_id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` int(255) NOT NULL,
+  `commision` int(255) NOT NULL,
+  `document` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip` int(255) NOT NULL,
+  `business_id` int(255) NOT NULL,
+  PRIMARY KEY (`staff_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `staff_details`
+--
+
+INSERT INTO `staff_details` (`staff_id`, `name`, `email`, `phone`, `commision`, `document`, `street`, `city`, `state`, `zip`, `business_id`) VALUES
+(1, 'ascd', 'krish@gmail.com', 1097, 5, '', 'Safce', '', 'sAD', 0, 4),
+(2, 'asfd', 'krish@gmail.com', 1234, 3, '', 'Safce', 'SAD', 'sAD', 0, 4),
+(3, 'krish', 'krish@gmail.com', 12345, 3, '', 'ASACd', 'aSd', 'ASd', 0, 3),
+(5, 'adade', 'dadef@ewfwf.fwf', 454488, 45, '', 'afafe', 'afgf', 'fwgwrg', 0, 4);
 
 -- --------------------------------------------------------
 
@@ -113,7 +205,11 @@ INSERT INTO `subscription_package` (`subs_id`, `subscription_name`, `subscriptio
 
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(255) NOT NULL AUTO_INCREMENT,
+  `logo_image` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `business_name` varchar(255) NOT NULL,
+  `type_business` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
@@ -122,16 +218,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `subscription_package` varchar(255) NOT NULL,
   `subscription_expdate` varchar(255) NOT NULL,
   `subscription_price` varchar(255) NOT NULL,
+  `pan` varchar(255) NOT NULL,
+  `adhaar` varchar(255) NOT NULL,
+  `business` varchar(255) NOT NULL,
+  `street` longtext NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `email`, `password`, `phone`, `country`, `status`, `date`, `subscription_package`, `subscription_expdate`, `subscription_price`) VALUES
-(4, 'deepbratt@gmail.com', 'aaa', '123123', 'India', 'pending', '1489688597', '', '', ''),
-(5, 'indro@new.com', 'rimuloveindu', '1097', 'India', 'pending', '1500054577', '', '', '');
+INSERT INTO `users` (`user_id`, `logo_image`, `username`, `email`, `business_name`, `type_business`, `password`, `phone`, `country`, `status`, `date`, `subscription_package`, `subscription_expdate`, `subscription_price`, `pan`, `adhaar`, `business`, `street`, `city`, `state`, `zip`) VALUES
+(4, '3611favicon.png', 'deep', 'deepbratt@gmail.com', '', '', 'aaa', '123123', 'India', 'pending', '1489688597', '', '', '', '4543654', '12345acb', '123456', '12345acb', '12345acb', '12345acb', '12345'),
+(5, '', '', 'indro@new.com', '', '', 'rimuloveindu', '1097', 'India', 'pending', '1500054577', '', '', '', '', '', '', '', '', '', ''),
+(6, '721007.png', 'debashis nath', 'debashisnath1992@gmail.com', 'portme', 'Finance and Insurance', 'aaaa', '7074459217', '', '', '', '', '', '', '4565', '456465465', 'portme', 'afrg', 'frge', 'fwrge', '423534'),
+(7, '', '', 'dev@g.c', '51515151', 'Accounting Services', 'aaa', '7074459217', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+(8, '7621', 'debashis', 'fweqfwr@fewfw.qff', '46545646', 'Agriculture, Farming, Fishing and Forestry', 'aaa', '7074459217', '', '', '', '', '', '', 'dqfwef', 'qfqf', 'wfwv', 'fewgrw', 'ewfwrgrw', 'qfwefw', '4254');
 
 -- --------------------------------------------------------
 
@@ -146,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `user_access_levels` (
   `business_id` varchar(255) NOT NULL,
   `access_menu` varchar(255) NOT NULL,
   PRIMARY KEY (`access_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `user_access_levels`
@@ -154,7 +260,18 @@ CREATE TABLE IF NOT EXISTS `user_access_levels` (
 
 INSERT INTO `user_access_levels` (`access_id`, `user_id`, `access_type`, `business_id`, `access_menu`) VALUES
 (1, '4', 'admin', '1', 'all'),
-(2, '5', 'admin', '0', 'all');
+(2, '5', 'admin', '0', 'all'),
+(3, '6', 'admin', '0', 'all'),
+(4, '0', 'admin', '0', 'all'),
+(5, '0', 'admin', '0', 'all'),
+(6, '0', 'admin', '0', 'all'),
+(7, '0', 'admin', '0', 'all'),
+(8, '0', 'admin', '0', 'all'),
+(9, '0', 'admin', '0', 'all'),
+(10, '0', 'admin', '0', 'all'),
+(11, '0', 'admin', '0', 'all'),
+(12, '7', 'admin', '0', 'all'),
+(13, '0', 'admin', '0', 'all');
 
 -- --------------------------------------------------------
 
@@ -187,12 +304,11 @@ CREATE TABLE IF NOT EXISTS `vendors` (
   `added_by` varchar(255) NOT NULL,
   `business_id` varchar(255) NOT NULL,
   PRIMARY KEY (`vendor_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `vendors`
 --
 
 INSERT INTO `vendors` (`vendor_id`, `salutation`, `firstname`, `lastname`, `company_name`, `email`, `work_phone`, `mobile`, `website`, `billing_street`, `billing_city`, `billing_state`, `billing_zip`, `billing_country`, `shipping_street`, `shipping_city`, `shipping_state`, `shipping_zip`, `shipping_country`, `notes`, `date`, `added_by`, `business_id`) VALUES
-(1, 'Mr', 'Ram', 'Raheem', 'bakchodbaba incorp', '', '9230841054', '9230841054', 'www.syspider.com', '177 old calcutta road , 14no. railway gate , Barrackpore\r\n101/B A Road Anandapuri Barrackpore', 'kolkata', 'West Bengal', '700122', 'India', '177 old calcutta road , 14no. railway gate , Barrackpore\r\n101/B A Road Anandapuri Barrackpore', 'kolkata', 'West Bengal', '700122', 'India', 'bakchodi ki jay hoo.. asdasd asd a', '1492154399', '4', '1'),
-(2, 'Mr', 'test', 'test', 'asdasda', '', '9230841054', '9230841054', '', '177 old calcutta road , 14no. railway gate , Barrackpore\r\n101/B A Road Anandapuri Barrackpore', 'kolkata', 'West Bengal', '700122', 'India', '177 old calcutta road , 14no. railway gate , Barrackpore\r\n101/B A Road Anandapuri Barrackpore', 'kolkata', 'West Bengal', '700122', 'India', 'brand fac', '1492161060', '4', '1');
+(4, '1', 'abcde', 'xyz', 'portme', 'portme@bkp.com', '7557012578', '7074459217', 'portme.co', 'abcde', 'abcde', 'abcde', '012345', 'INDIA', 'abcde', 'abcde', 'abcde', '012345', 'INDIA', '', '07/16/2017 05:41:13', '4', '4');
